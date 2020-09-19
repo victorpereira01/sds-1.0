276990da323e1252171574c8b7b4aa67c48da321
Victor Pereira <victor.cordeiro2@gmail.com>
2020-09-19 17:27:10 -0300

    feat: dinamycally listing records

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -15,4 +15,4 @@ const Routes = () => (
     </BrowserRouter>
 );
 
-export default Routes;
+export default Routes; 
--- web/src/pages/Records/index.tsx
+++ web/src/pages/Records/index.tsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Records = () => (
-    <h1>Records</h1>
-)
+import api from '../../services/api';
+import { formatDate } from './helpers';
+import { RecordsResponse } from './types';
+
+import './styles.css';
+
+const Records = () => {
+
+    const [recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
+
+    useEffect(() => {
+        api.get('/records?linesPerPage=12')
+            .then(res => setRecordsResponse(res.data));
+    }, [])
+
+
+    return (
+        <div className="page-container">
+            <table className="records-table" cellPadding="0" cellSpacing="0">
+                <thead>
+                    <tr>
+                        <th>INSTANTE</th>
+                        <th>NOME</th>
+                        <th>IDADE</th>
+                        <th>PLATAFORMA</th>
+                        <th>GÊNERO</th>
+                        <th>TÍTULO DO GAME</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {recordsResponse?.content.map(record => (
+                        <tr key={record.id}>
+                            <td>{formatDate(record.moment)}</td>
+                            <td>{record.name}</td>
+                            <td>{record.age}</td>
+                            <td className="text-secondary">{record.gamePlatform}</td>
+                            <td>{record.genreName}</td>
+                            <td className="text-primary">{record.gameTitle}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
 
 export default Records;
