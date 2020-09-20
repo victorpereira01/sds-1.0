766121488bd6eaaa9dd5eb4d8793f1cf15d70b74
Victor Pereira <victor.cordeiro2@gmail.com>
2020-09-20 17:52:49 -0300

    feat: home page created

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { AppLoading } from 'expo';
+import { useFonts, Play_400Regular, Play_700Bold } from '@expo-google-fonts/play';
+
+import Routes from './src/routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const [fontsLoaded] = useFonts({
+    Play_400Regular,
+    Play_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      <View style={styles.container}>
+        <Routes />
+        <StatusBar style="light" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
 });
